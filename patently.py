fe80b14e16077ab14a06d8f7d0265b03466461ee
Medhya Goel <71662879+medhyaGoel@users.noreply.github.com>
2024-04-07 11:07:04 -0700

    Initial commit

--- patently.py
+++ patently.py
@@ -1,5 +1,9 @@
 import streamlit as st
+from streamlit.logger import get_logger
 import side_bar
+import openai
+
+logger = get_logger(__name__)
 
 side_bar.sidebar()
 openai_api_key = st.session_state.get("OPENAI_API_KEY")
@@ -11,6 +15,26 @@ if not openai_api_key:
 
 st.title('patently')
 st.subheader('Figure out whether your invention is already out there.')
+
+def is_open_ai_key_valid(openai_api_key, model: str) -> bool:
+    if model == "debug":
+        return True
+
+    if not openai_api_key:
+        st.error("Please enter your OpenAI API key in the sidebar!")
+        return False
+    try:
+        openai.ChatCompletion.create(
+            model=model,
+            messages=[{"role": "user", "content": "test"}],
+            api_key=openai_api_key,
+        )
+    except Exception as e:
+        st.error(f"{e.__class__.__name__}: {e}")
+        logger.error(f"{e.__class__.__name__}: {e}")
+        return False
+
+    return True
 
 my_inv = st.file_uploader(
     "Upload a description of your invention!",
@@ -27,6 +51,9 @@ other_inv = st.file_uploader(
 if not my_inv or (not my_inv and not other_inv):
     st.stop()
 
-# if not is_open_ai_key_valid(openai_api_key, model):
-#     st.stop()
+
+
+if not is_open_ai_key_valid(openai_api_key, model):
+    st.stop()
+
 
